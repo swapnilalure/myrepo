5fc25a0578f55bbcc9ab0c5ccd6191bafe7545bb
swapnilalure <103204661+swapnilalure@users.noreply.github.com>
2022-04-07 21:12:00 +0530

    practice projects

---
+++ First.java
@@ -0,0 +1,12 @@
+package projectJava;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi from Java by Swapnil");
+		System.out.println("Hi again");
+
+	}
+
+}
---
+++ Operations.java
@@ -0,0 +1,30 @@
+package projectJava;
+
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//addition code
+		int a, b, c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println("addition = "+c);
+		//subtraction code
+		c=b-a;
+		System.out.println("Subtraction = "+c);
+		//division code
+		c=b/a;
+		System.out.println("Division = "+c);
+		//multiplication code
+		c=a*b;
+		System.out.println("Multiplication = "+c);
+		//modulus code
+		c=a%b;
+		System.out.println("Reminder = "+c);
+		
+	}
+	//cmd command compile: javac file_name.java
+	//cmd command to run: java file_name
+
+}
